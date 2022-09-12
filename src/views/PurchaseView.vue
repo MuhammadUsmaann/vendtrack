15abd59a37d2773da74a177b088db75b85251423
raza awan <razaawan.1033@gmail.com>
2022-09-12 11:15:44 +0500

    changes

--- src/views/PurchaseView.vue
+++ src/views/PurchaseView.vue
@@ -444,7 +444,7 @@ export default {
 
 }
 </script>
-<style scoped>
+<style scoped >
 .arrowsRight {
   right: 15px;
 }
--- src/views/WOWView.vue
+++ src/views/WOWView.vue
@@ -7,23 +7,28 @@
         </CHeading>
       </div>
       <div class="col px-0 text-right">
-        <span class="font-500 font-14 font-mono mr-1 w-md-100 d-inline-block mb-md-0 mb-2">Last Update: 02/18/2022</span>
+        <span class="font-500 font-14 font-mono mr-1 w-md-100 d-inline-block mb-md-0 mb-2">Last Update:
+          02/18/2022</span>
         <b-dropdown id="dropdown-1" text="All Sales" variant="outline"
-          class=" mt-2 d-inline-block inner-border-light-gray ml-md-2 inner-font-14 font-weight-500 font-black shadow-sm bg-transparent">
+          class=" d-inline-block inner-border-light-gray ml-md-2 inner-font-14 font-weight-500 font-black shadow-sm bg-transparent">
           <b-dropdown-item>First Action</b-dropdown-item>
           <b-dropdown-item>Second Action</b-dropdown-item>
           <b-dropdown-item>Third Action</b-dropdown-item>
         </b-dropdown>
         <b-dropdown id="dropdown-1" text="Cases" variant="outline"
-          class=" mt-2 d-inline-block inner-border-light-gray ml-2 inner-font-14 font-weight-500 font-black shadow-sm bg-transparent">
+          class=" d-inline-block inner-border-light-gray ml-2 inner-font-14 font-weight-500 font-black shadow-sm bg-transparent">
           <b-dropdown-item>First Action</b-dropdown-item>
           <b-dropdown-item>Second Action</b-dropdown-item>
           <b-dropdown-item>Third Action</b-dropdown-item>
         </b-dropdown>
-        <button class=" mt-2 btn border border-light-gray ml-2 font-14 font-weight-500 font-black shadow-sm"><img
-            src="../assets/excel.png" alt="" class="pr-1"> <div class="d-md-inline-block d-none"> Export in Excel</div></button>
-        <button class=" mt-2 btn border border-light-gray ml-2 font-14 font-weight-500 font-black shadow-sm"><img
-            src="../assets/pdf.png" alt="" class="pr-1"> <div class="d-md-inline-block d-none"> Export in PDF</div></button>
+        <button class=" btn border border-light-gray ml-2 font-14 font-weight-500 font-black shadow-sm"><img
+            src="../assets/excel.png" alt="" class="pr-1">
+          <div class="d-md-inline-block d-none"> Export in Excel</div>
+        </button>
+        <button class=" btn border border-light-gray ml-2 font-14 font-weight-500 font-black shadow-sm"><img
+            src="../assets/pdf.png" alt="" class="pr-1">
+          <div class="d-md-inline-block d-none"> Export in PDF</div>
+        </button>
       </div>
     </div>
     <b-row class="mb-4 mx-0">
